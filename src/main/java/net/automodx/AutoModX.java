2740ac13947f9b53e86d13f900ecf5d48c504725
ihateyou <czumbilkonrad2009@gmail.com>
2024-12-23 17:37:57 +0100

    Update AutoModX.java

--- src/main/java/net/automodx/AutoModX.java
+++ src/main/java/net/automodx/AutoModX.java
@@ -17,7 +17,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import net.automodx.listener.ChatListener;
 
 import net.automodx.commands.AddUserCommand;
 import net.automodx.commands.AddWordCommand;
