ccf662e16195a24c483cb45a855582bac9cc578c
ihateyou <czumbilkonrad2009@gmail.com>
2024-12-23 15:10:43 +0100

    Create ChatListener.java

---
+++ src/main/java/net/automodx/listener/ChatListener.java
@@ -0,0 +1,40 @@
+package net.automodx;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class ChatListener implements Listener {
+
+    private final AutoModX plugin;
+
+    public ChatListener(AutoModX plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+    
+        if (plugin.getIgnoredUsers().contains(event.getPlayer().getName().toLowerCase())) {
+            return;
+        }
+
+        
+        String message = event.getMessage().toLowerCase();
+
+
+        for (String bannedWord : plugin.getBannedWords()) {
+            if (message.contains(bannedWord.toLowerCase())) {
+                
+                event.setCancelled(true);
+
+                
+                event.getPlayer().sendMessage(ChatColor.RED + "You cannot use the word '" + bannedWord + "'.");
+
+                
+                return;
+            }
+        }
+    }
+}
